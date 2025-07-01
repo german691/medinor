55d975fe5902adf611c36ea827a9e47812cc02aa
Pablo Delgado <113070544+german691@users.noreply.github.com>
2025-07-01 06:58:00 -0300

    localhost

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./src/app.js";
 
 const PROTOCOL = process.env.SSL || "http";
-const SERVER_IP = process.env.SERVER_IP || "192.168.16.111";
+const SERVER_IP = process.env.SERVER_IP || "localhost";
 const SERVER_PORT = process.env.SERVER_PORT || "5000";
 
 const url = `${PROTOCOL}://${SERVER_IP}:${SERVER_PORT}`;
